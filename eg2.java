7f3b7ce215cebf672cb38f125a8a25bd45920699
Ramhiya K <111469995+Ramhiya@users.noreply.github.com>
2023-07-11 21:46:44 +0530

    Create eg2.java

---
+++ eg2.java
@@ -0,0 +1,10 @@
+numbers = []
+for i in range(5):
+    number = int(input("Enter an integer: "))
+    numbers.append(number)
+
+# Calculation
+sum_of_numbers = sum(numbers)
+
+# result
+print("The sum of the numbers is:", sum_of_numbers)
