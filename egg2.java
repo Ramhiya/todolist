c32d4effbf3e570c0cf858058da8de1944f470fd
Ramhiya K <111469995+Ramhiya@users.noreply.github.com>
2023-07-21 19:03:13 +0530

    Create egg2.java

---
+++ egg2.java
@@ -0,0 +1,18 @@
+function findMax(numbers) {
+  if (!Array.isArray(numbers) || numbers.length === 0) {
+    throw new Error('Input must be a non-empty array of numbers.');
+  }
+
+  let max = numbers[0];
+
+  for (let i = 1; i < numbers.length; i++) {
+    if (numbers[i] > max) {
+      max = numbers[i];
+    }
+  }
+
+  return max;
+}
+const numbers = [3, 7, 1, 9, 5, 2];
+const maxNumber = findMax(numbers);
+console.log(maxNumber); // Output: 9
